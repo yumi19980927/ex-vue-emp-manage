602fe08675563537dbf129589750746dd32b0a30
igamasayuki <igamasayuki@gmail.com>
2021-11-08 07:07:33 +0900

    App.vueをTypeScript版に修正

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
